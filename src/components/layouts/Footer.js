1f21c8b1c847c5255963c421b9e27b2282f558d4
Tottzi <tottzi@gmail.com>
2021-07-06 17:44:37 +0200

    github link changing

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -3,7 +3,7 @@ import { GoMarkGithub } from 'react-icons/go';
 const Footer = () => (
     <footer className='footer'>
       <a className='footer__link'
-      href='https://github.com/Tottzi'
+      href='https://github.com/Tottzi/reduxtodo'
       target='_blank'>
         <GoMarkGithub className='footer__icon'/>
       </a>
